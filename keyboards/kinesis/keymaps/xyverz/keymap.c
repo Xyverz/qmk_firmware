8e7d1d74ac3a525018810fd136bf5e049df07d64
Ian Sterling <xyverz@gmail.com>
2021-03-27 11:31:52 -0700

    Rework of Kinesis keymap
    In preparation of setting up my Dactyl, I'm updating my Kinesis
    keymap to match what I envision for my Dactyl.
    
    I've removed the Qwerty, Colemak, & keypad layers, added a raise
    and lower layer, and put the Fn keys on the number row in the raise
    and lower layers.

--- keyboards/kinesis/keymaps/xyverz/keymap.c
+++ keyboards/kinesis/keymaps/xyverz/keymap.c
@@ -13,6 +13,9 @@ enum custom_keycodes { DVORAK = SAFE_RANGE, QWERTY, COLEMAK, KEYPAD };
 #define GUI_END GUI_T(KC_END)
 #define KPD_ENT LT(_KEYPAD, KC_ENT)
 #define MACLOCK LGUI(LCTL(KC_Q))
+#define WINLOCK LGUI(KC_L)
+#define LOWER MT(_LOWER)
+#define RAISE MT(_RAISE)
 
 // clang-format off
 
@@ -23,215 +26,153 @@ enum custom_keycodes { DVORAK = SAFE_RANGE, QWERTY, COLEMAK, KEYPAD };
 	|  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   F7   |  F8   |
  	`-----------------------------------------------------------------'
         ,-----------------------------------------------------------------.
-        |  F9  |  F10 |  F11 |  F12 | PScr | SLck | Paus | Keypad | RESET |
+        |  F9  |  F10 |  F11 |  F12 | PScr | SLck | Paus | Unused | RESET |
         `-----------------------------------------------------------------'
 
         Dvorak layer:
 	,-------------------------------------------.,-------------------------------------------.
-	|   =    |   1  |   2  |   3  |   4  |   5  ||   6  |   7  |   8  |   9  |   0  |   \    |
+	| Escape |   1  |   2  |   3  |   4  |   5  ||   6  |   7  |   8  |   9  |   0  |   \    |
 	|--------+------+------+------+------+------||------+------+------+------+------+--------|
 	| Tab    |   '  |   ,  |   .  |   P  |   Y  ||   F  |   G  |   C  |   R  |   L  |   /    |
 	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| CapsLk |   A  |   O  |   E  |   U  |   I  ||   D  |   H  |   T  |   N  |   S  |   -    |
+	| ~      |   A  |   O  |   E  |   U  |   I  ||   D  |   H  |   T  |   N  |   S  |   -    |
 	|--------+------+------+------+------+------||------+------+------+------+------+--------|
 	| LShift |   Z  |   X  |   C  |   V  |   X  ||   B  |   M  |   W  |   V  |   Z  | RShift |
 	`--------+------+------+------+------+------'`------+------+------+------+------+--------'
-	         |   `  |  INS | Left | Rght |              |  Up  |  Dn  |   [  |   ]  |
+	         |   `  | Left | Rght |Lower |              |Raise |  Up  |  Dn  |   =  |
 	         `---------------------------'              `---------------------------'
 	                             ,--------------.,--------------.
 	                             | LCtl  | LAlt || RGUI | RCtl  |
 	                      ,------|-------|------||------+-------+-------.
-	                      |      |       | Home || PgUp | Enter |       |
-	                      | BkSp |  Del  |------||------|   /   | Space |
-	                      |      |       | End  || PgDn | KeyPd |       |
+	                      |      |       | Home || PgUp |       |       |
+	                      | BkSp |  Del  |------||------| Enter | Space |
+	                      |      |       | End  || PgDn |       |       |
 	                      `---------------------'`----------------------'
 
-        QWERTY layer:
+        Lower layer:
 	,-------------------------------------------.,-------------------------------------------.
-	|   =    |   1  |   2  |   3  |   4  |   5  ||   6  |   7  |   8  |   9  |   0  |   -    |
+	| F11    |  F1  |  F2  |  F3  |  F4  |  F5  ||  F6  |  F7  |  F8  |  F9  | F10  |  F12   |
 	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| Tab    |   Q  |   W  |   E  |   R  |   T  ||   Y  |   U  |   I  |   O  |   P  |   \    |
+	|        |      |      |      |      |      ||      |      |      |      |      |        |
 	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| CapsLk |   A  |   S  |   D  |   F  |   G  ||   H  |   J  |   K  |   L  |   ;  |   '    |
+	| CapsLk |      | Mute | Vol- | Vol+ |      ||      |      | Plus | LCBR | RCBR |        |
 	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| LShift |   Z  |   X  |   C  |   V  |   B  ||   N  |   M  |   ,  |   .  |   /  | RShift |
+	|        | Stop | Prev | Play | Next | Sel  ||      |      |      |      |      |        |
 	`--------+------+------+------+------+------'`------+------+------+------+------+--------'
-	         |   `  |  INS | Left | Rght |              |  Up  |  Dn  |   [  |   ]  |
-	         `---------------------------'              `---------------------------'
-	                             ,--------------.,--------------.
-	                             | LCtl  | LAlt || RGUI | RCtl  |
-	                      ,------|-------|------||------+-------+-------.
-	                      |      |       | Home || PgUp | Enter |       |
-	                      | BkSp |  Del  |------||------|   /   | Space |
-	                      |      |       | End  || PgDn | KeyPd |       |
-	                      `---------------------'`----------------------'
-
-        Colemak layer:
-	,-------------------------------------------.,-------------------------------------------.
-	|   =    |   1  |   2  |   3  |   4  |   5  ||   6  |   7  |   8  |   9  |   0  |   -    |
-	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| Tab    |   Q  |   W  |   F  |   P  |   G  ||   J  |   L  |   U  |   Y  |   ;  |   \    |
-	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| BkSpc  |   A  |   R  |   S  |   T  |   D  ||   H  |   N  |   E  |   I  |   O  |   '    |
-	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| LShift |   Z  |   X  |   C  |   V  |   B  ||   K  |   M  |   ,  |   .  |   /  | RShift |
-	`--------+------+------+------+------+------'`------+------+------+------+------+--------'
-	         |   `  |  INS | Left | Rght |              |  Up  |  Dn  |   [  |   ]  |
-	         `---------------------------'              `---------------------------'
-	                             ,--------------.,--------------.
-	                             | LCtl  | LAlt || RGUI | RCtl  |
-	                      ,------|-------|------||------+-------+-------.
-	                      |      |       | Home || PgUp | Enter |       |
-	                      | BkSp |  Del  |------||------|   /   | Space |
-	                      |      |       | End  || PgDn | KeyPd |       |
-	                      `---------------------'`----------------------'
-
-        Keypad layer:
-	,-------------------------------------------.,-------------------------------------------.
-	| Power  |      |      |      |      |      ||      | NmLk | KP = | KP / | KP * |        |
-	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| Sleep  |      |      |      |      |      ||      | KP 7 | KP 8 | KP 9 | KP - |        |
-	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	| Wake   |      | Mute | Vol- | Vol+ |      ||      | KP 4 | KP 5 | KP 6 | KP + |        |
-	|--------+------+------+------+------+------||------+------+------+------+------+--------|
-	|        | Stop | Prev | Play | Next | Sel  ||      | KP 1 | KP 2 | KP 3 |KP Ent|        |
-	`--------+------+------+------+------+------'`------+------+------+------+------+--------'
-	         |      |QWERTY|Colemk|Dvorak|              |      |      | KP . |KP Ent|
+	         |      |      |      |      |              |      |      |      |      |
 	         `---------------------------'              `---------------------------'
 	                              ,-------------.,-------------.
-	                              |      |      ||      |MacLck|
+	                              |      |      ||      |      |
 	                       ,------|------|------||------+------+------.
 	                       |      |      |      ||      |      |      |
-	                       |      |      |------||------|      | KP 0 |
-	                       |      |      |MacLck||      |      |      |
+	                       |      |      |------||------|      | Ins  |
+	                       |      |      |MacLck||WinLck|      |      |
 	                       `--------------------'`--------------------'
-*/
+
+        Raise layer:
+	,-------------------------------------------.,-------------------------------------------.
+	| F11    |  F1  |  F2  |  F3  |  F4  |  F5  ||  F6  |  F7  |  F8  |  F9  | F10  |  F12   |
+	|--------+------+------+------+------+------||------+------+------+------+------+--------|
+	|        |      |      |      |      |      ||      |      |      |      |      |        |
+	|--------+------+------+------+------+------||------+------+------+------+------+--------|
+	| CapsLk |      | Mute | Vol- | Vol+ |      ||      |      | Eql  | LBRC | RBRC |        |
+	|--------+------+------+------+------+------||------+------+------+------+------+--------|
+	|        | Stop | Prev | Play | Next | Sel  ||      |      |      |      |      |        |
+	`--------+------+------+------+------+------'`------+------+------+------+------+--------'
+	         |      |      |      |      |              |      |      |      |      |
+	         `---------------------------'              `---------------------------'
+	                              ,-------------.,-------------.
+	                              |      |      ||      |      |
+	                       ,------|------|------||------+------+------.
+	                       |      |      |      ||      |      |      |
+	                       |      |      |------||------|      | Ins  |
+	                       |      |      |MacLck||WinLck|      |      |
+	                       `--------------------'`--------------------'
+                           */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_DVORAK] = LAYOUT (
            // Left Hand
            KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
-           KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+           KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
            KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,
-           KC_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,
+           KC_TILD, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,
            KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,
-                    KC_GRV,  KC_INS,  KC_LEFT, KC_RGHT,
+                    KC_GRV,  KC_LEFT, KC_RGHT, LOWER,
            // Left Thumb
                     KC_LCTL, KC_LALT,
                              KC_HOME,
            KC_BSPC, KC_DEL,  GUI_END,
 
            // Right Hand
-           KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, TG(_KEYPAD),  RESET,
+           KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, XXXXXXX,  RESET,
            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
            KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
            KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
            KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
-                    KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
+                    RAISE,   KC_UP,   KC_DOWN, KC_EQL,
            // Right Thumb
            KC_RGUI, KC_RCTL,
            KC_PGUP,
-           KC_PGDN, KPD_ENT, KC_SPC
+           KC_PGDN, KC_ENT,  KC_SPC
     ),
 
-[_QWERTY] = LAYOUT (
-           // Left Hand
-           KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
-           KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-           KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-           KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-                    KC_GRV,  KC_INS,  KC_LEFT, KC_RGHT,
-           // Left Thumb
-                    KC_LCTL, KC_LALT,
-                             KC_HOME,
-           KC_BSPC, KC_DEL,  KC_END,
-
-           // Right Hand
-           KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, TG(_KEYPAD),  RESET,
-           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS ,
-           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
-           // Right Thumb
-           KC_RGUI, KC_RCTL,
-           KC_PGUP,
-           KC_PGDN, KPD_ENT, KC_SPC
-    ),
-
-[_COLEMAK] = LAYOUT (
-           // Left Hand
-           KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
-           KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-           KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,
-           KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,
-           KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-                    KC_GRV,  KC_INS,  KC_LEFT, KC_RGHT,
-           // Left Thumb
-                    KC_LCTL, KC_LALT,
-                             KC_HOME,
-           KC_BSPC, KC_DEL,  KC_END,
-
-           // Right Hand
-           KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, TG(_KEYPAD),  RESET,
-           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-           KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-           KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-           KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
-           // Right Thumb
-           KC_RGUI, KC_RCTL,
-           KC_PGUP,
-           KC_PGDN, KPD_ENT, KC_SPC
-    ),
-
-[_KEYPAD] = LAYOUT (
+[_LOWER] = LAYOUT (
            // Left Hand
            _______, _______, _______, _______, _______, _______, _______, _______, _______,
-           KC_PWR,  _______, _______, _______, _______, _______,
-           KC_SLEP, _______, _______, _______, _______, _______,
-           KC_WAKE, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+           KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
+           _______, _______, _______, _______, _______, _______,
+           KC_CAPS, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
            _______, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSEL,
-                    _______, QWERTY,  COLEMAK, DVORAK,
+                    _______, _______, _______, _______,
            // Left Thumb
                     _______, _______,
                              _______,
            _______, _______, MACLOCK,
 
            // Right Hand
-           _______, _______, _______, _______, _______, _______, _______, _______, _______,
-           _______, KC_NLCK, KC_PEQL, KC_PSLS, KC_PAST, _______,
-           _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
-           _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
-           _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______,
-                    _______, _______, KC_PDOT, KC_PENT,
+           _______, _______, _______, _______, _______, _______, _______, XXXXXXX, _______,
+           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+           _______, _______, _______, _______, _______, _______,
+           _______, _______, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+           _______, _______, _______, _______, _______, _______,
+                    _______, _______, _______, _______,
            // Right Thumb
-           _______, MACLOCK,
+           _______, _______,
            _______,
-           _______, _______, KC_P0
+           WINLOCK, _______, _______
+    )
+};
+
+[_RAISE] = LAYOUT (
+           // Left Hand
+           _______, _______, _______, _______, _______, _______, _______, _______, _______,
+           KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
+           _______, _______, _______, _______, _______, _______,
+           KC_CAPS, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+           _______, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSEL,
+                    _______, _______, _______, _______,
+           // Left Thumb
+                    _______, _______,
+                             _______,
+           _______, _______, MACLOCK,
+
+           // Right Hand
+           _______, _______, _______, _______, _______, _______, _______, XXXXXXX, _______,
+           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+           _______, _______, _______, _______, _______, _______,
+           _______, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+           _______, _______, _______, _______, _______, _______,
+                    _______, _______, _______, _______,
+           // Right Thumb
+           _______, _______,
+           _______,
+           WINLOCK, _______, _______
     )
 };
 
 // clang-format on
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch (keycode) {
-            case DVORAK:
-                set_single_persistent_default_layer(_DVORAK);
-                return false;
-            case QWERTY:
-                set_single_persistent_default_layer(_QWERTY);
-                return false;
-            case COLEMAK:
-                set_single_persistent_default_layer(_COLEMAK);
-                return false;
-        }
-    }
-    return true;
-};
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void){};
