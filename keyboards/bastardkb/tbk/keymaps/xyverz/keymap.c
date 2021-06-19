9fcbec40a2bbe321d8b2ae98ae565511013632f0
Ian Sterling <xyverz@gmail.com>
2021-06-19 00:59:32 -0700

    Updates for TBK keymap
    Added a layer for games that won't let me use Backspace or Delete
    as mappable keys.

--- keyboards/bastardkb/tbk/keymaps/xyverz/keymap.c
+++ keyboards/bastardkb/tbk/keymaps/xyverz/keymap.c
@@ -17,9 +17,9 @@
 
 #include QMK_KEYBOARD_H
 
-enum layer_names { _WINDOWS, _MAC, _LOWER, _RAISE, _ADJUST };
+enum layer_names { _WINDOWS, _MAC, _WEIRDGAMES, _LOWER, _RAISE, _ADJUST };
 
-enum custom_keycodes { WINDOWS = SAFE_RANGE, MAC, ADJUST };
+enum custom_keycodes { WINDOWS = SAFE_RANGE, MAC, WGAMES, ADJUST };
 
 // Aliases to keep the keymap tidy
 #define GUIBSPC GUI_T(KC_BSPC)  // GUI when held, BSPC when tapped.
@@ -54,6 +54,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       KC_LCTL, KC_LALT,     KC_RGUI, KC_RCTL
   ),
 
+[_WEIRDGAMES] = LAYOUT_split_4x6_5(
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+  KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+                             LOWER,   KC_SPC,  KC_LGUI,     KC_ENT,  KC_SPC,  RAISE,
+                                      KC_LCTL, KC_LALT,     KC_RGUI, KC_RCTL
+  ),
+
 [_LOWER] = LAYOUT_split_4x6_5(
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, 
   RESET,   _______, _______, KC_UP,   _______, _______,     _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,
@@ -75,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_split_4x6_5(
   _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
   _______, RESET,   _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,     _______, WINDOWS, MAC,     _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,     _______, WINDOWS, MAC,     WGAMES,  _______, _______,
   _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
                              _______, _______, _______,     _______, _______, _______,
                                       _______, _______,     _______, _______
@@ -93,6 +102,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             case MAC:
                 set_single_persistent_default_layer(_MAC);
+                return false;
+            case WGAMES:
+                set_single_persistent_default_layer(_WEIRDGAMES);
                 return false;
         }
     }
